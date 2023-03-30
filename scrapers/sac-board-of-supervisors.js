9c9866d32db2737cc48c9bc052f04400287e7058
Jeremia Kimelman <jbkimelman@gmail.com>
2023-03-30 09:26:40 -0700

    Fix timezone for County BOS

--- scrapers/sac-board-of-supervisors.js
+++ scrapers/sac-board-of-supervisors.js
@@ -6,46 +6,61 @@ const url = "https://saccounty.mhsoftware.com/rss/calendar_id/3.xml"
 
 const parseDate = timeParse("%a, %e %b %Y %_I:%M:%S %Z")
 
-async function scrapeSacBoardOfSupervisors() {
-  const req = await fetch(url)
-  const text = await req.text()
-  const $ = cheerio.load(text, { xmlMode: true })
+function getStartArrayAdjustedForTimezone(date) {
+    const formattingOptions = {
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZone: 'America/Los_Angeles'
+    }
+    const adjusted = new Intl.DateTimeFormat('en-us', formattingOptions).format(date)
+    const [datePart, timePart] = adjusted.split(', ')
+    const [month, day, year] = datePart.split('/')
+    const [hourMinutePart, partOfDay] = timePart.split(' ')
+    const [hour, minute] = hourMinutePart.split(':')
+    const start = [+year, +month, +day,
+        partOfDay === 'PM' ? +hour + 12 : +hour, +minute,
 
-  const items = $("item")
-
-  const data = []
-  items.each(function (index, item) {
-    const $$ = $(this)
-
-    const title = $$.find("title").text()
-    const description = $$.find("description").text()
-    const link = $$.find("link").text()
-    const guid = $$.find("guid").text()
-    const startDate = $$.find("cdaily\\:eventStartDate").text()
-
-    const isBoardMeeting = title.toLowerCase().includes("board of supervisors")
-    if (!isBoardMeeting) return
-
-    const date = parseDate(startDate)
-    const start = [
-      date.getFullYear(),
-      date.getMonth() + 1,
-      date.getDate(),
-      date.getHours(),
-      date.getMinutes(),
     ]
+    return start
+}
 
-    data.push({
-      title: "Sac County Board of Supervisors Meeting",
-      description: `Sign up to take notes for this meeting at https://www.socialjusticesac.org/wix-sac-bos`,
-      url: link,
-      uid: guid,
-      start,
-      duration: { hours: 2 },
+async function scrapeSacBoardOfSupervisors() {
+    const req = await fetch(url)
+    const text = await req.text()
+    const $ = cheerio.load(text, { xmlMode: true })
+
+    const items = $("item")
+
+    const data = []
+    items.each(function(index, item) {
+        const $$ = $(this)
+
+        const title = $$.find("title").text()
+        const description = $$.find("description").text()
+        const link = $$.find("link").text()
+        const guid = $$.find("guid").text()
+        const startDate = $$.find("cdaily\\:eventStartDate").text()
+
+        const isBoardMeeting = title.toLowerCase().includes("board of supervisors")
+        if (!isBoardMeeting) return
+
+        const date = parseDate(startDate)
+        const start = getStartArrayAdjustedForTimezone(date)
+
+        data.push({
+            title: "Sac County Board of Supervisors Meeting",
+            description: `Sign up to take notes for this meeting at https://www.socialjusticesac.org/wix-sac-bos`,
+            url: link,
+            uid: guid,
+            start,
+            duration: { hours: 2 },
+        })
     })
-  })
 
-  return data
+    return data
 }
 
 module.exports = scrapeSacBoardOfSupervisors
